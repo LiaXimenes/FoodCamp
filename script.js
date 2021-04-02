8c82734a3f031cd1e269affeea74c5fd8ebe19d4
Lia Ximenes <ximeneslia@gmail.com>
2021-04-02 02:47:11 -0300

    inclusão de função em todos os 9 pratos

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-function meclica(){
-
-    if (itemselecionado != null){
-        const prato_excluido = document.querySelector(".item, .steak_au_gratin")
-        prato_excluido.classList.remove("verde");
-    }
-
-
+function comidas(){
     const comida = document.querySelector(".pratos");
-    comida.classList.toggle("verde");
+    comida.classList.add("verde");
+
+    const botao =document.querySelector(".ion");
+    botao.classList.remove("escondido");   
+}
+
+
+function bebida(){
+    const bebidas = document.querySelector(".lata");
+    bebidas.classList.add("verde");
+
+    const boto =document.querySelector(".ions");
+    boto.classList.remove("escondido");   
+}
+
+function sobremesa(){
+    const sobremesas = document.querySelector(".doce");
+    sobremesas.classList.add("verde");
+
+    const botao =document.querySelector(".ionss");
+    botao.classList.remove("escondido");   
+}
+
+
+
+
+
+
+if (itemselecionado != null){
+    const comida = document.querySelector(".pratos")
+    comida.classList.remove("verde");
 }

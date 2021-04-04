0fa7bb89e33c730d85c191fd3c38932aa973c24f
Lia Ximenes <ximeneslia@gmail.com>
2021-04-04 01:48:59 -0300

    tentativa de acertar bordas

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function escolha(numero){
+function comida(numero){
     const naoverde = document.querySelector(".verde")
         if (naoverde != null){    
            naoverde.classList.remove("verde");
@@ -6,12 +6,42 @@ function escolha(numero){
     const selecionado = "." + numero;
     const comida = document.querySelector(selecionado);
     comida.classList.add("verde");
-
-   
-   // no de cima eu tiro a classe, em baixo eu teria que recolocar a coisa para deixar de aparecer o botão,
-   //é o inverso, como faz isso? 
-   
-    const botao = document.querySelector(".ion" + numero);
-    botao.classList.remove("escondido");   
+  
 }
 
+function bebida(numero){
+    const naoverde = document.querySelector(".verde")
+        if (naoverde != null){    
+           naoverde.classList.remove("verde");
+        }
+    const selecionado = "." + numero;
+    const comida = document.querySelector(selecionado);
+    comida.classList.add("verde");
+  
+}
+
+
+function doce(numero){
+    const naoverde = document.querySelector(".verde")
+        if (naoverde != null){    
+           naoverde.classList.remove("verde");
+        }
+    const selecionado = "." + numero;
+    const comida = document.querySelector(selecionado);
+    comida.classList.add("verde");
+  
+}
+
+
+
+
+
+// function botaofinal(){
+
+//     if (ao selecionar os 3 pratos o botão tem que ser ativado){
+        
+//         botaofim.classList.add("finalizacao-verde")
+//      innerHTML("Fechar Pedido")
+
+//     }
+// }
